82cbffabe353719eb787dd854933d13a82ea9b75
paulo-flores <paulorgs.flores@gmail.com>
2020-12-12 16:06:30 -0300

    Add Card and ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
         home: Scaffold(
+      body: Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text('100,00'),
+          subtitle: Text('1000'),
+        ),
+      ),
       appBar: AppBar(
         title: Text('Transferências'),
       ),
