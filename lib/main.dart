769422fdcb057ace9b7c2789c9a9af09f2a4db62
paulo <paulo@pop-os.localdomain>
2020-12-26 10:55:23 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
         home: Scaffold(
-      body: Card(
-        child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text('100,00'),
-          subtitle: Text('1000'),
-        ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('100,00'),
+              subtitle: Text('1000'),
+            ),
+          ),
+           Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('250,00'),
+              subtitle: Text('1900'),
+            ),
+          ),
+        ],
       ),
       appBar: AppBar(
         title: Text('Transferências'),
